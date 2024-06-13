b73c122ad7791c7cd53b7937b5a15669bd8cd9fd
AlexBaldauff <ajb00057@mix.wvu.edu>
2024-06-13 15:16:33 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CS 230L</h1>
+      <h2>Section - 702</h2>
+      <p>WVY ID: 800382882</p>
+      <p>Hi I am Alex</p>
+    </div>
+  );
+}
+
+export default App;

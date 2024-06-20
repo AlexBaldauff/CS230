d6c85a519f9848aed0b442a105c54d6f5b952c44
AlexBaldauff <ajb00057@mix.wvu.edu>
2024-06-20 14:27:19 -0400

    Add navbar and cards modify app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar.js';
+import Card from './Card.js';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
+
+<Navbar/>
+
       <h1>CS 230L</h1>
       <h2>Section - 702</h2>
       <p>WVY ID: 800382882</p>
       <p>Hi I am Alex</p>
+
+<Card/>
+
     </div>
   );
 }

5e48008fe51d149a79d2b1e69f29f848f9ce6ef9
AlexBaldauff <ajb00057@mix.wvu.edu>
2024-06-20 14:25:34 -0400

    Added Navbar and Cards

---
+++ src/Card.js
@@ -0,0 +1,32 @@
+function Card(){
+    return(
+<div class="row">
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Card 1</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Card 2</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Card 3</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+</div>
+)
+}
+
+export default Card;
